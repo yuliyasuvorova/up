88c24c41ff1987b2a8f53fce78aec686ac03f02d
Yuliya Suvorova <juliasuvorovaah@gmail.com>
2024-11-13 10:44:29 +0100

    up

---
+++ stringhe.py
@@ -0,0 +1,10 @@
+n="hello world"
+print(n.split(sep=","))
+print(n.upper())
+print(n.lower())
+print(n.swapcase())
+f=("this is python")
+print(n + "!! " + f)
+print(b"ciao")
+if (f.endswith("!")):print ("True") 
+else:print("False")
---
+++ test.py
@@ -0,0 +1,9 @@
+n = 0
+
+while n < 10:
+    print(n)
+    n=n+1
+
+
+nome = "Yuliya"
+print(f"ciao {nome}")
